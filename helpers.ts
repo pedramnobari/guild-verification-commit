8c0791d1efd1bc2627d64b5b040d144e8ae4c47d
pedramnobari <80333330+pedramnobari@users.noreply.github.com>
2025-10-31 13:30:40 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T10:30:00.106Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-10-31T10:30:39.861Z
+Commit: Fix broken links in documentation
